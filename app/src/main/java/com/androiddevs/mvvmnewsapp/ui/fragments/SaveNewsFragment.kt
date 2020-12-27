c8187ba5c60861f92d323059e28fbc3ccb8203cb
ahmeddiderrahat <adrahat.neub@gmail.com>
2020-12-27 18:30:45 +0600

    DONE-Article fragment data loading with WebView on 27-DEC-2020 at 18:60

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SaveNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SaveNewsFragment.kt
@@ -3,19 +3,45 @@ package com.androiddevs.mvvmnewsapp.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.adapter.NewsAdapter
 import com.androiddevs.mvvmnewsapp.ui.NewsActivity
 import com.androiddevs.mvvmnewsapp.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
 
 class SaveNewsFragment : Fragment(R.layout.fragment_saved_news) {
     private lateinit var viewModel: NewsViewModel
+
+    private val TAG = "BreakingNewsFragment"
+
+    private lateinit var newsAdapter: NewsAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = (activity as NewsActivity).viewModel
 
+        setupRecylerView()
 
+        newsAdapter.setOnItemClickedListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(
+                R.id.action_saveNewsFragment_to_articleFragment,
+                bundle
+            )
+        }
+    }
+
+    private fun setupRecylerView() {
+        newsAdapter = NewsAdapter()
+        rvBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 
 }
